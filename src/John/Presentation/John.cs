a71fa83faaa01ecb98263e15c423c45a22c86e20
Carlos Montes <carlosmontes002@gmail.com>
2024-01-14 16:50:17 -0300

    initial commit

---
+++ src/John/Presentation/John.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class John : CharacterBody2D
+{
+	[Export] private float Speed = 300.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Y = direction.Y * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
---
+++ src/MollysHouse/Presentation/MollysHouse.cs
@@ -0,0 +1,7 @@
+using Godot;
+
+namespace gtfo_demons.MollysHouse.Presentation; 
+
+public partial class MollysHouse : Node {
+	
+}
